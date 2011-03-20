9cc2fef15cf0c26df85e555448781715bb9fa136
Ben Floyd <dataplex@gmail.com>
2011-03-20 16:49:47 -0500

    Adding custom dictionary example

---
+++ CustomDictionaryExample/ExampleHPWEB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomDictionaryExample
+{
+    /// <summary>
+    /// HPWEB is a necessary acronym for this system
+    /// </summary>
+    public class ExampleHPWEB
+    {
+    }
+}
